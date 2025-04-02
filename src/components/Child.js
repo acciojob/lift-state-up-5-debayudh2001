0a5127ced97964fcfcfae6cc672d34725de203f7
debayudh2001 <debayudh31@gmail.com>
2025-04-02 11:46:00 +0530

    first commit

---
+++ src/components/Child.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Child = ({setIsLoggedIn}) => {
+    function handleSubmit(e){
+        e.preventDefault()
+        setIsLoggedIn(true)
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username:</label>&nbsp;
+                <input type="text" name="username" />
+                <br />
+                <br />
+                <label htmlFor="password">Password:</label>&nbsp;
+                <input type="password" name="password" />
+                <br />
+                <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Child
